e8e9cae01e444738c73c254ef25a32a026196d85
YashKhatpe <yashkhatpe0611@gmail.com>
2023-12-20 15:41:48 +0530

    First Commit

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017";
-
-const connectToMongo = ()=> {
-    mongoose.connect("mongodb://localhost:27017/")
-    console.log("Connected to MongoDb Sucessfully");
-}
-
-module.exports = connectToMongo;
